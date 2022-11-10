c4f2a02d2bff2d57c08309299bd1832d254b3965
MikeChara <mikejchara@gmail.com>
2022-11-10 18:41:39 +0000

    last task left

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -11,14 +11,16 @@ function Main() {
 
   return (
     <main id="main">
-    {/* using the keys of the object as a param by destructuring to have neater code */}
-      {posts.map(({postId,title,date,author,text}) => (
+      {/* using the keys of the object as a param by destructuring to have neater code */}
+      {posts.map(({ postId, title, date, author, text, highlights, image }) => (
         <Post
           key={postId}
           title={title}
           date={date}
           author={author}
           text={text}
+          highlights={highlights}
+          image={image}
         />
       ))}
     </main>
@@ -29,4 +31,7 @@ export default Main;
 
 // goal is to loop over postState with a .map
 // for each visit of the array indexes ensure componant is rendered.
+// THINGS LEFT.☑️
+// Make the image work.☑️
+//Make the highlights work with a map.
 //
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -5,10 +5,11 @@ import "./index.css";
 function Post({ title, date, author, text, hightlights, image }) {
   return (
     <article>
-      title={title}
-      date={date}
-      author={author}
-      text={text}
+      <h1>{title}</h1>
+      <h3>{date}</h3>
+      <img src={image.link} alt={image.alt} width={"50%"}></img>
+      <h3>{author}</h3>
+      <p>{text}</p>
     </article>
   );
 }
